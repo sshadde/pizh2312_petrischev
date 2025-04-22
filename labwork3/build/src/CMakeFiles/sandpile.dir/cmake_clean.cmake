add45e83e32c473af467da5eff06acc83b98efb6
yaroslav <petrichevjaroslav@gmail.com>
2025-04-23 00:07:05 +0300

    CMake expand

---
+++ labwork3/build/src/CMakeFiles/sandpile.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sandpile.dir/link.d"
+  "CMakeFiles/sandpile.dir/args_parser.cpp.o"
+  "CMakeFiles/sandpile.dir/args_parser.cpp.o.d"
+  "CMakeFiles/sandpile.dir/bmp_writer.cpp.o"
+  "CMakeFiles/sandpile.dir/bmp_writer.cpp.o.d"
+  "CMakeFiles/sandpile.dir/main.cpp.o"
+  "CMakeFiles/sandpile.dir/main.cpp.o.d"
+  "CMakeFiles/sandpile.dir/sandpile.cpp.o"
+  "CMakeFiles/sandpile.dir/sandpile.cpp.o.d"
+  "sandpile"
+  "sandpile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sandpile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ labwork3/build/src/CMakeFiles/sandpile.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for sandpile.
+# This may be replaced when dependencies are built.
