07f61d4dd25d2fdc2010e80f340046871b5ac473
yaroslav <petrichevjaroslav@gmail.com>
2025-04-22 23:40:48 +0300

    successful build

---
+++ labwork3/build/CMakeFiles/sandpile.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yaroslav/pizh2312_petrischev/labwork3/src/args_parser.cpp" "CMakeFiles/sandpile.dir/src/args_parser.cpp.o" "gcc" "CMakeFiles/sandpile.dir/src/args_parser.cpp.o.d"
+  "/home/yaroslav/pizh2312_petrischev/labwork3/src/bmp_writer.cpp" "CMakeFiles/sandpile.dir/src/bmp_writer.cpp.o" "gcc" "CMakeFiles/sandpile.dir/src/bmp_writer.cpp.o.d"
+  "/home/yaroslav/pizh2312_petrischev/labwork3/src/main.cpp" "CMakeFiles/sandpile.dir/src/main.cpp.o" "gcc" "CMakeFiles/sandpile.dir/src/main.cpp.o.d"
+  "/home/yaroslav/pizh2312_petrischev/labwork3/src/sandpile.cpp" "CMakeFiles/sandpile.dir/src/sandpile.cpp.o" "gcc" "CMakeFiles/sandpile.dir/src/sandpile.cpp.o.d"
+  "" "sandpile" "gcc" "CMakeFiles/sandpile.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ labwork3/build/CMakeFiles/sandpile.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sandpile.dir/link.d"
+  "CMakeFiles/sandpile.dir/src/args_parser.cpp.o"
+  "CMakeFiles/sandpile.dir/src/args_parser.cpp.o.d"
+  "CMakeFiles/sandpile.dir/src/bmp_writer.cpp.o"
+  "CMakeFiles/sandpile.dir/src/bmp_writer.cpp.o.d"
+  "CMakeFiles/sandpile.dir/src/main.cpp.o"
+  "CMakeFiles/sandpile.dir/src/main.cpp.o.d"
+  "CMakeFiles/sandpile.dir/src/sandpile.cpp.o"
+  "CMakeFiles/sandpile.dir/src/sandpile.cpp.o.d"
+  "sandpile"
+  "sandpile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sandpile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
