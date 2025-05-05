f3451ddcb7632ba4854c2f2370e3ebb1d7bb262c
yaroslav <petrichevjaroslav@gmail.com>
2025-05-05 15:33:33 +0300

    ArgParser.h public added

--- labwork5/lib/ArgParser.h
+++ labwork5/lib/ArgParser.h
@@ -1,9 +1,57 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <functional>
+#include <memory>
+#include <unordered_map>
+#include <sstream>
 
 namespace ArgumentParser {
 
 class ArgParser {
-    // Your Implementation here!
+public:
+    explicit ArgParser(const std::string& program_name);
+
+    // создание аргументов
+    ArgParser& AddStringArgument(const std::string& name, const std::string& description = "");
+    ArgParser& AddStringArgument(char short_name, const std::string& long_name, const std::string& description = "");
+
+    ArgParser& AddIntArgument(const std::string& name, const std::string& description = "");
+    ArgParser& AddIntArgument(char short_name, const std::string& long_name, const std::string& description = "");
+
+    ArgParser& AddFlag(const std::string& long_name, const std::string& description = "");
+    ArgParser& AddFlag(char short_name, const std::string& long_name, const std::string& description = "");
+
+    ArgParser& AddHelp(char short_name, const std::string& long_name, const std::string& description = "");
+
+    // модификаторы последнего добавленного аргумента
+    ArgParser& Default(const std::string& val);
+    ArgParser& Default(const char* val);
+    ArgParser& Default(int val);
+    ArgParser& Default(bool val);
+
+    ArgParser& MultiValue(size_t min_count = 0);
+    ArgParser& Positional();
+
+    ArgParser& StoreValue(std::string& ref);
+    ArgParser& StoreValue(int& ref);
+    ArgParser& StoreValue(bool& ref);
+
+    ArgParser& StoreValues(std::vector<std::string>& ref);
+    ArgParser& StoreValues(std::vector<int>& ref);
+
+    // парсинг
+    bool Parse(int argc, char** argv);
+    bool Parse(const std::vector<std::string>& args);
+
+    // помощь
+    bool Help() const;
+    std::string HelpDescription() const;
+
+    // геттеры
+    std::string GetStringValue(const std::string& name) const;
+    int GetIntValue(const std::string& name, size_t idx = 0) const;
+    bool GetFlag(const std::string& name) const;
 };
 
 } // namespace ArgumentParser
