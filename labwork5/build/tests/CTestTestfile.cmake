afdb8ce407989467bd2538704f9e4b0a55735336
yaroslav <petrichevjaroslav@gmail.com>
2025-05-06 18:51:54 +0300

    build directory deleted

--- labwork5/build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yaroslav/pizh2312_petrischev/labwork5/tests
-# Build directory: /home/yaroslav/pizh2312_petrischev/labwork5/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/yaroslav/pizh2312_petrischev/labwork5/build/tests/argparser_tests[1]_include.cmake")
-subdirs("../_deps/googletest-build")
